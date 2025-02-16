e099d106abc07a02aa2f8c58b5651e0f79aa8b19
komori <L2409855575as@gmail.com>
2025-02-16 10:59:08 +0900

    第21回演習課題

--- src/main/java/raisetech/studentManagement/controller/StudentController.java
+++ src/main/java/raisetech/studentManagement/controller/StudentController.java
@@ -22,48 +22,46 @@ import raisetech.studentManagement.date.StudentsCourses;
 import raisetech.studentManagement.domain.StudentDetail;
 import raisetech.studentManagement.service.StudentService;
 
+/**
+ * 受講生の検索や登録、更新などを行うREST APIとして受け付けるControllerです。
+ */
 @RestController
 public class StudentController {
 
   private StudentService service;
-  private StudentConverter converter;
 
   @Autowired
-  public StudentController(StudentService service, StudentConverter converter) {
+  public StudentController(StudentService service) {
     this.service = service;
-    this.converter = converter;
   }
 
+  /**
+   * 受講生一覧検索です。
+   * 全件検索を行うので、条件指定は行いません。
+   *
+   * @return 受講生一覧（全件）
+   */
   @GetMapping("/studentList")
   public List<StudentDetail> getStudentList() {
-    List<Student> students = service.searchStudentList();
-    List<StudentsCourses> studentsCourses = service.searchStudentCourseList();
-    return converter.convertStudentDetails(students, studentsCourses);
+    return service.searchStudentList();
   }
 
+  /**
+   * 受講生検索です。
+   * IDに紐づく任意の受講生情報を取得します。
+   *
+   * @param id 　受講生ID
+   * @return 受講生
+   */
   @GetMapping("/student/{id}")
-  public String getStudent(@PathVariable int id, Model model) {
-    StudentDetail studentDetail = service.searchStudent(id);
-    model.addAttribute("studentDetail", studentDetail);
-    return "updateStudent";
+  public StudentDetail getStudent(@PathVariable int id) {
+    return service.searchStudent(id);
   }
-
-  @GetMapping("/newStudent")
-  public String newStudent(Model model) {
-    final StudentDetail studentDetail = new StudentDetail();
-    studentDetail.setStudentsCourses(Arrays.asList(new StudentsCourses()));
-    model.addAttribute("studentDetail", studentDetail);
-    return "registerStudent";
-  }
-
 
   @PostMapping("/registerStudent")
-  public String registerStudent(@ModelAttribute StudentDetail studentDetail, BindingResult result) {
-    if (result.hasErrors()) {
-      return "registerStudent";
-    }
-    service.registerStudent(studentDetail);
-    return "redirect:/studentList";
+  public ResponseEntity<StudentDetail> registerStudent(@RequestBody StudentDetail studentDetail) {
+    StudentDetail responseStudentDetail = service.registerStudent(studentDetail);
+    return ResponseEntity.ok(responseStudentDetail);
   }
 
 
--- src/main/java/raisetech/studentManagement/repository/StudentRepository.java
+++ src/main/java/raisetech/studentManagement/repository/StudentRepository.java
@@ -10,31 +10,42 @@ import raisetech.studentManagement.date.Student;
 import raisetech.studentManagement.date.StudentsCourses;
 
 /**
- * 受講生情報を扱うリポジトリ。
- * <p>
- * 全件検索や単一条件での検索、コース情報の検索が行えるクラスです。
+ * 受講生テーブルと受講生コース情報テーブルと紐づくRepositoryです。
  */
 @Mapper
 public interface StudentRepository {
 
   /**
-   * 全件検索します。
+   * 受講生の全件検索を行います。
    *
-   * @return　全件検索した受講生情報の一覧
+   * @return 受講生一覧（全件）
    */
-  //生徒全件検索
   @Select("SELECT * FROM students")
   List<Student> search();
 
-  //生徒単一検索
+  /**
+   * 受講生の検索を行います。
+   *
+   * @param id 受講生ID
+   * @return 受講生
+   */
   @Select("SELECT * FROM students WHERE id = #{id}")
   Student searchStudent(int id);
 
-  //コース全件検索
+  /**
+   * 受講生のコース情報の全件検索を行います。
+   *
+   * @return 受講生のコース情報（全件）
+   */
   @Select("SELECT * FROM students_courses")
   List<StudentsCourses> searchStudentsCoursesList();
 
-  //コース単一検索
+  /**
+   * 受講生IDに紐づく受講生コース情報を検索します。
+   *
+   * @param studentId 受講生ID
+   * @return 受講生IDに紐づく受講生コース情報
+   */
   @Select("SELECT * FROM students_courses WHERE student_id = #{studentId}")
   List<StudentsCourses> searchStudentsCourses(int studentId);
 
@@ -44,12 +55,11 @@ public interface StudentRepository {
   @Options(useGeneratedKeys = true, keyProperty = "id")
   void insertStudent(Student student);
 
-  @Insert("INSERT INTO students_courses (student_id,course,start_date,expected_completion_date)"
+  @Insert("INSERT INTO students_courses (student_id, course, start_date, expected_completion_date)"
       + "VALUES (#{studentId}, #{course}, #{startDate}, #{expectedCompletionDate})")
   @Options(useGeneratedKeys = true, keyProperty = "id")
   void insertStudentCourse(StudentsCourses studentsCourses);
 
-  //更新
   @Update(
       "UPDATE students SET full_name = #{fullName}, furigana = #{furigana}, nick_name = #{nickname},"
           + "email_address = #{emailAddress}, area = #{area}, age = #{age}, sex = #{sex}, remark = #{remark}, is_deleted =#{isDeleted} WHERE id = #{id}")
