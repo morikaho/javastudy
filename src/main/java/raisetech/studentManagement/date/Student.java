cfe759a67f28c2ceec4a84308f8c16594b083a9b
komori <L2409855575as@gmail.com>
2025-02-25 21:23:23 +0900

    第25回演習課題

--- src/main/java/raisetech/studentManagement/date/Student.java
+++ src/main/java/raisetech/studentManagement/date/Student.java
@@ -1,40 +1,53 @@
 package raisetech.studentManagement.date;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.Setter;
 
+@Schema(description = "受講生")
 @Getter
 @Setter
 public class Student {
 
+  @Schema(description = "受講生ID", example = "1234")
   @NotBlank
   @Pattern(regexp = "^\\d+$")
   private String id;
 
+  @Schema(description = "受講生の名前", example = "山田　太郎")
   @NotBlank
   private String fullName;
 
+  @Schema(description = "受講生のふりがな", example = "やまだ　たろう")
   @NotBlank
   private String furigana;
 
+  @Schema(description = "受講生のニックネーム", example = "たろう")
   @NotBlank
   private String nickname;
 
+  @Schema(description = "受講生のメールアドレス", example = "example@example.com")
   @NotBlank
   @Email
   private String emailAddress;
 
+  @Schema(description = "受講生の住んでいる地域（市区町村）", example = "東京都新宿区西新宿")
   @NotBlank
   private String area;
 
+  @Schema(description = "受講生の年齢", example = "20")
   private int age;
 
+  @Schema(description = "受講生の性別", example = "男")
   @NotBlank
   private String sex;
 
+  @Schema(description = "備考", example = "特になし")
   private String remark;
+
+  @Schema(description = "削除フラグ（true: 削除済み, false: 有効）", example = "false")
   private boolean isDeleted;
 }
--- src/main/java/raisetech/studentManagement/date/StudentCourse.java
+++ src/main/java/raisetech/studentManagement/date/StudentCourse.java
@@ -1,26 +1,34 @@
 package raisetech.studentManagement.date;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import java.time.LocalDate;
 import lombok.Getter;
 import lombok.Setter;
 
+@Schema(description = "受講生コース情報")
 @Getter
 @Setter
 public class StudentCourse {
 
+  @Schema(description = "コースID", example = "1234")
   @NotBlank
   @Pattern(regexp = "^\\d+$")
   private String id;
 
+  @Schema(description = "受講生のID", example = "1234")
   @NotBlank
   @Pattern(regexp = "^\\d+$")
   private String studentId;
 
+  @Schema(description = "コース名", example = "JAVAコース")
   @NotBlank
   private String course;
 
+  @Schema(description = "受講開始日", example = "2024-01-01")
   private LocalDate startDate;
+
+  @Schema(description = "受講終了日", example = "2024-04-01")
   private LocalDate expectedCompletionDate;
 }
