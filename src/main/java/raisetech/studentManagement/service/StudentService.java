4edb593c033a781556d3014f1e7f1ecfd9902cb2
komori <L2409855575as@gmail.com>
2025-03-06 06:36:58 +0900

    第27回演習課題

--- src/main/java/raisetech/studentManagement/service/StudentService.java
+++ src/main/java/raisetech/studentManagement/service/StudentService.java
@@ -74,7 +74,7 @@ public class StudentService {
    * @param studentCourse 受講生コース情報
    * @param student       受講生
    */
-  private void initStudentCourse(StudentCourse studentCourse, Student student) {
+  void initStudentCourse(StudentCourse studentCourse, Student student) {
     final LocalDate now = LocalDate.now();
 
     studentCourse.setStudentId(student.getId());
---
+++ src/test/java/raisetech/studentManagement/service/StudentServiceTest.java
@@ -0,0 +1,109 @@
+package raisetech.studentManagement.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import raisetech.studentManagement.controller.converter.StudentConverter;
+import raisetech.studentManagement.date.Student;
+import raisetech.studentManagement.date.StudentCourse;
+import raisetech.studentManagement.domain.StudentDetail;
+import raisetech.studentManagement.repository.StudentRepository;
+
+@ExtendWith(MockitoExtension.class)
+class StudentServiceTest {
+
+  @Mock
+  private StudentRepository repository;
+
+  @Mock
+  private StudentConverter converter;
+
+  private StudentService sut;
+
+  @BeforeEach
+  void before() {
+    sut = new StudentService(repository, converter);
+  }
+
+  @Test
+  void 受講生詳細の一覧機能_リポジトリとコンバーターの処理が適切に呼び出せていること() {
+    List<Student> studentList = new ArrayList<>();
+    List<StudentCourse> studentCourseList = new ArrayList<>();
+    when(repository.search()).thenReturn(studentList);
+    when(repository.searchStudentCourseList()).thenReturn(studentCourseList);
+
+    sut.searchStudentList();
+
+    verify(repository, times(1)).search();
+    verify(repository, times(1)).searchStudentCourseList();
+    verify(converter, times(1)).convertStudentDetails(studentList, studentCourseList);
+  }
+
+  @Test
+  void 受講生詳細の検索_リポジトリの処理が適切に呼び出せていること() {
+    Student student = new Student();
+    List<StudentCourse> studentCourse = new ArrayList<>();
+    String id = "100";
+    student.setId(id);
+    when(repository.searchStudent(id)).thenReturn(student);
+    when(repository.searchStudentCourse(id)).thenReturn(studentCourse);
+
+    StudentDetail expected = new StudentDetail(student, studentCourse);
+
+    StudentDetail actual = sut.searchStudent(id);
+
+    verify(repository, times(1)).searchStudent(id);
+    verify(repository, times(1)).searchStudentCourse(id);
+    assertEquals(actual.getStudent().getId(), expected.getStudent().getId());
+  }
+
+  @Test
+  void 受講生詳細の登録_リポジトリの処理が適切に呼び出せていること() {
+    Student student = new Student();
+    StudentCourse studentCourse = new StudentCourse();
+    List<StudentCourse> studentCourseList = new ArrayList<>(List.of(studentCourse));
+    StudentDetail studentDetail = new StudentDetail(student, studentCourseList);
+
+    sut.registerStudent(studentDetail);
+
+    verify(repository, times(1)).insertStudent(student);
+    verify(repository, times(1)).insertStudentCourse(studentCourse);
+  }
+
+  @Test
+  void 受講生の登録_受講生コース情報の初期情報の設定が適切に行われていること() {
+    Student student = new Student();
+    student.setId("100");
+    StudentCourse studentCourse = new StudentCourse();
+    LocalDate now = LocalDate.now();
+
+    sut.initStudentCourse(studentCourse, student);
+
+    assertEquals(student.getId(), studentCourse.getStudentId());
+    assertEquals(studentCourse.getStartDate(), now);
+    assertEquals(studentCourse.getExpectedCompletionDate(), now.plusYears(1));
+  }
+
+  @Test
+  void 受講生の更新_リポジトリの処理が適切に呼び出せていること() {
+    Student student = new Student();
+    StudentCourse studentCourse = new StudentCourse();
+    List<StudentCourse> studentCourseList = new ArrayList<>(List.of(studentCourse));
+    StudentDetail studentDetail = new StudentDetail(student, studentCourseList);
+
+    sut.updateStudent(studentDetail);
+
+    verify(repository, times(1)).updateStudent(studentDetail.getStudent());
+    verify(repository, times(1)).updateStudentCourse(studentCourse);
+  }
+}
